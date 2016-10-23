22928aae0bf3b9f76e6ff46f27d4f2f2a453a5d9
Mikael <miketekle14@gmail.com>
2016-10-22 17:00:24 -0700

    created mask for animation

--- TwitterSplashScreenAnimation/ViewController.swift
+++ TwitterSplashScreenAnimation/ViewController.swift
@@ -7,21 +7,26 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var twitterFeed: UIImageView!
-
+    
+    var mask: CALayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //set up mask
+        self.mask = CALayer()
+        self.mask?.contents = UIImage(named: "twitter logo mask")
+        self.mask?.contentsGravity = kCAGravityResizeAspect
+        self.mask?.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.mask?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.mask?.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height/2)
+        
     }
-
 
 }
 
